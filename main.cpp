a99c1c626839772152c818a0658231fb1abd63ce
circutrider21 <57774961+circutrider21@users.noreply.github.com>
2020-02-22 18:45:00 +0000

    added namespace

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include "toml.hpp"
 
+using namespace std;
+
 int main(int argc, char *argv[]) {
   parser p = new parser();
-  String toparse = argv[1];
+  string toparse = argv[1];
   p.argone(toparse);
 }
