0f3022a2d7aad9730cb8d032e7100b993b48f037
circutrider21 <57774961+circutrider21@users.noreply.github.com>
2020-02-22 17:30:43 +0000

    Finished The pares for the most part. Now to add testing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "toml.hpp"
 
-int main() {
-  
+int main(int argc, char *argv[]) {
+  parser p = new parser();
+  String toparse = argv[1];
+  p.argone(toparse);
 }
--- parser.hpp
+++ parser.hpp
@@ -4,8 +4,22 @@ using namespace std;
 
 public class parser {
   public:
-  int parse() {
+  int parse(String command) {
     const auto data = toml::parse("build.toml");
-
+    const auto physical = toml::find(data, command);
+    const auto color = toml::find<std::string>(physical, "run");
+    system(color);
+  }
+  void argone(String t) {
+    if (t.compare("version") == 0) {
+      cout << "1.3.0" << "\n";
+    } else if (t.compare("help") == 0) {
+      cout << "USAGE: Exodus <command>\n";
+    } else if (t.compare("about") == 0) {
+      cout << "\tExodus is a command line build tool written in C++ \nand designed to simplify the process of compiling small coding projects\nExodus is extensible and able to be extended into other compilers and tools\nFinnally Exodus uses ToruNiina's Toml Parser to parse the toml conf files\n"
+      cout << "Copyright 2020 Peeled Fruit Studios Under The Mit License\n";
+    } else {
+      parse(argv[1]);
+    }
   }
 }
